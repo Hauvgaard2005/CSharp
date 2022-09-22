bde6d550d60de7529cab6b56b109ec7381679f5b
Hauvgaard <math214x@mail.hansenberg.dk>
2022-09-22 08:16:20 +0200

    Fibonacci fix
    i fix

--- 5_Assignment/Program.cs
+++ 5_Assignment/Program.cs
@@ -1,19 +1,17 @@
 ﻿//Un-comment below code and fix the issues
 //Push to github
-
-/*
-int RecursiveFibonacci(int first, int second, int count)
-{
-    count--;
-    
-    if(count == 0) {
-        
-    }else{
-        return RecursiveFibonacci(?, ?, ?);
-    }
+int nextFibo; 
+int RecursiveFibonacci(int first, int second, int count) { 
+    count--; 
+    nextFibo = first + second; 
+    first = second; 
+    second = nextFibo; 
+    if (count == 0) { 
+        return nextFibo; 
+    } else { 
+        return RecursiveFibonacci(first, second, count); 
+    } 
 }
-
-int lastFibo = RecursiveFibonacci(1,1,5);
-Console.WriteLine("Last fibo number was " + lastFibo);
-Console.ReadLine(); 
-*/
+int lastFibo = RecursiveFibonacci(1, 1, 5); 
+Console.WriteLine("Last fibo number was " + lastFibo); 
+Console.ReadLine();
